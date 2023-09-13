752fa02275cc3582a15d716c34e363d99bb683fe
Cristian-Veliz <cristianveliz6@gmail.com>
2023-09-13 15:25:41 -0300

    Plantilla base client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto Final</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.module.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>¡Estoy en mi Home!</h1>
+    </div>
+  );
+}
+
+export default Home;
